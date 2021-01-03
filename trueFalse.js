becba209aea97f55411ca0cdf3a454727041ce92
yasirstk <yasirstk@users.noreply.github.com>
2021-01-03 12:44:47 +0600

    added

---
+++ trueFalse.js
@@ -0,0 +1,10 @@
+//Falsy = false, 0, "", undefined, null, NaN, null
+//Truthy = '0', ' ', [], 'false', etc
+const age = 12;
+
+if(age){
+    console.log('Condition is true');
+}
+else{
+    console.log('Condition is false');
+}
